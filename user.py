c4dadc22246ac528ab55bf4d4d96520250cc0047
Ornella-KK <ornellakankazi@gmail.com>
2020-11-15 19:06:41 +0200

    created a delete user method

--- user.py
+++ user.py
@@ -9,3 +9,6 @@ class User:
         save_user method saves user objects into user_list
         '''
         User.user_list.append(self)
+    
+    def delete_user(self):
+        User.user_list.remove(self)
