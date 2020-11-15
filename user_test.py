7e9fcb0b479aba2987c660bb6098c1590a37e711
Ornella-KK <ornellakankazi@gmail.com>
2020-11-15 18:52:37 +0200

    added a save_contact

--- user_test.py
+++ user_test.py
@@ -11,3 +11,17 @@ class TestUser(unittest.TestCase):
         test_init test case to test if the object is initialized properly
         '''
         self.assertEqual(self.new_user.user_name,"Ornella")
+    
+    def test_save_user(self):
+        self.new_user.save_contact()
+        self.assertEqual(len(User.user_list),1)
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+
+
+if __name__ == '__main__':
+    unittest.main()
