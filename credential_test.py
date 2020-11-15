3349316becbdd4ec3bbaea5e6a25232f30171bff
Ornella-KK <ornellakankazi@gmail.com>
2020-11-15 19:50:08 +0200

    added delete and display all test

--- credential_test.py
+++ credential_test.py
@@ -8,17 +8,37 @@ class TestCredential(unittest.TestCase):
     def test_init(self):
         self.assertEqual(self.new_credential.password,"4556")
 
-    def test_save_contact(self):
+    def test_save_credential(self):
         self.new_credential.save_credential()
         self.assertEqual(len(Credential.credential_list),1)
 
     def tearDown(self):
         Credential.credential_list = []
 
-    def test_save_multiple_contact(self):
+    def test_save_multiple_credential(self):
         self.new_credential.save_credential()
         test_credential = Credential("4556")
         test_credential.save_credential()
         self.assertEqual(len(Credential.credential_list),2)
+        
+    def test_delete_credential(self):
+        self.new_credential.save_credential()
+        test_credential = Credential("4556")
+        test_credential.save_credential()
+
+        self.new_credential.delete_credential()
+        self.assertEqual(len(Credential.credential_list),1)
+
+    def test_credential_exists(self):
+        self.new_credential.save_credential()
+        test_credential = Credential("4556")
+        test_credential.save_credential()
+
+        credential_exists = Credential.credential_list("4556")
+        self.assertTrue(credential_exists)
+
+    def test_display_all_credential(self):
+        self.assertEqual(Credential.display_credential(),Credential.credential_list)
+
 if __name__ == '__main__':
     unittest.main()
